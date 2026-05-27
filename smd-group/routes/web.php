ce4169d77cf9fd370e355867ac89563dd693d7d2
usha <webusha28@gmail.com>
2026-05-27 13:41:51 +0530

    fav icon and logo update

--- smd-group/routes/web.php
+++ smd-group/routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
+
+Route::get('/optimize', function () {
+    Artisan::call('optimize:clear');
+    return "optimized";
+});
 
 Route::get('/', function () {
     // return view('welcome');
