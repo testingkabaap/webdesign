49e5f59926c26f649b041f017974f9af66eb7b2a
usha <webusha28@gmail.com>
2026-06-26 15:55:18 +0530

    change for home page

--- smd-group/resources/views/template/head.blade.php
+++ smd-group/resources/views/template/head.blade.php
@@ -2,3 +2,5 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" ></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" ></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js"></script>
