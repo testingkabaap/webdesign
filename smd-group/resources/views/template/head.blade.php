0f286d2947c3d701067d949b57f716f683822cf6
usha <webusha28@gmail.com>
2026-07-10 12:43:06 +0530

    whatsapp chat

--- smd-group/resources/views/template/head.blade.php
+++ smd-group/resources/views/template/head.blade.php
@@ -23,7 +23,7 @@
 <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
  <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
+    {{-- <script type="text/javascript">
         var Tawk_API = Tawk_API || {},
             Tawk_LoadStart = new Date();
         (function() {
@@ -35,5 +35,5 @@
             s1.setAttribute('crossorigin', '*');
             s0.parentNode.insertBefore(s1, s0);
         })();
-    </script>
+    </script> --}}
     <!--End of Tawk.to Script-->
